e9ddb4ae788baf713040dbf3cc9c96b9a3299dd0
duol9 <lhyy0195@gmail.com>
2025-02-12 01:35:45 +0900

    :sparkles: feat: Add validation message for biddingPrice in CreateBidReq

--- src/main/java/nbc/mushroom/domain/bid/dto/request/CreateBidReq.java
+++ src/main/java/nbc/mushroom/domain/bid/dto/request/CreateBidReq.java
@@ -3,7 +3,7 @@ package nbc.mushroom.domain.bid.dto.request;
 import jakarta.validation.constraints.NotNull;
 
 public record CreateBidReq(
-    @NotNull
+    @NotNull(message = "biddingPrice는 필수 값 입니다. ")
     Long biddingPrice
 ) {
 
