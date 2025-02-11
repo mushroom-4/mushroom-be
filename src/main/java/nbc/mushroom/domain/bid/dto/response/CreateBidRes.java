326e8c1d41713db18fa50cad05d7adc902df99d2
duol9 <lhyy0195@gmail.com>
2025-02-12 01:43:07 +0900

    :sparkles: feat: Add 'from' method to CreateBidRes for entity transformation

--- src/main/java/nbc/mushroom/domain/bid/dto/response/CreateBidRes.java
+++ src/main/java/nbc/mushroom/domain/bid/dto/response/CreateBidRes.java
@@ -1,5 +1,6 @@
 package nbc.mushroom.domain.bid.dto.response;
 
+import nbc.mushroom.domain.bid.entity.Bid;
 import nbc.mushroom.domain.bid.entity.BiddingStatus;
 
 public record CreateBidRes(
@@ -7,4 +8,10 @@ public record CreateBidRes(
     BiddingStatus biddingStatus
 ) {
 
+    public static CreateBidRes from(Bid bid) {
+        return new CreateBidRes(
+            bid.getBiddingPrice(),
+            bid.getBiddingStatus()
+        );
+    }
 }
