6d578b825c5b79bf2bcff05ab827007a1047143f
answerin1 <dabin990723@naver.com>
2025-02-12 00:12:17 +0900

    :sparkles: feat: Add new error codes

--- src/main/java/nbc/mushroom/domain/common/exception/ExceptionType.java
+++ src/main/java/nbc/mushroom/domain/common/exception/ExceptionType.java
@@ -20,9 +20,10 @@ public enum ExceptionType {
     EMAIL_DUPLICATE(HttpStatus.BAD_REQUEST, "U02", "이메일이 중복됩니다."),
     PASSWORD_NOT_MATCH(HttpStatus.BAD_REQUEST, "U03", "기존 비밀번호가 일치하지 않습니다."),
     PASSWORD_SAME(HttpStatus.BAD_REQUEST, "U04", "새 비밀번호는 기존 비밀번호와 같을 수 없습니다."),
-    INVALID_USER_ROLE(HttpStatus.BAD_REQUEST, "U05", "유효하지 않은 UserRole")
+    INVALID_USER_ROLE(HttpStatus.BAD_REQUEST, "U05", "유효하지 않은 UserRole"),
 
     //Product
+    PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "P01", "해당 상품을 찾을 수 없습니다.")
 
     // Bid
 
