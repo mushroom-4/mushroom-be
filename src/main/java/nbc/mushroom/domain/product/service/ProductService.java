25fa97cce208b6b1f3cf8c7c557eafe5aff5615b
answerin1 <dabin990723@naver.com>
2025-02-12 00:10:56 +0900

    :sparkles: feat: Implement product detail view service

--- src/main/java/nbc/mushroom/domain/product/service/ProductService.java
+++ src/main/java/nbc/mushroom/domain/product/service/ProductService.java
@@ -1,6 +1,10 @@
 package nbc.mushroom.domain.product.service;
 
 import lombok.RequiredArgsConstructor;
+import nbc.mushroom.domain.common.exception.CustomException;
+import nbc.mushroom.domain.common.exception.ExceptionType;
+import nbc.mushroom.domain.product.dto.response.searchProductRes;
+import nbc.mushroom.domain.product.entity.Product;
 import nbc.mushroom.domain.product.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,4 +15,11 @@ import org.springframework.transaction.annotation.Transactional;
 public class ProductService {
 
     private final ProductRepository productRepository;
+
+    public searchProductRes searchProductRes(long productId) {
+        Product searchProduct = productRepository.findById(productId)
+            .orElseThrow(() -> new CustomException(ExceptionType.PRODUCT_NOT_FOUND));
+        return searchProductRes.from(searchProduct);
+    }
+
 }
