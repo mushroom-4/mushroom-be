c12e29d12ac6c86c4b5b2fe927a43794d7b8394f
duol9 <lhyy0195@gmail.com>
2025-02-12 01:45:54 +0900

    :sparkles: feat: Add custom query method to find Bid by User and Product
    - Optional.orElseGet()을 사용하기 위해 Optional<T>타입으로 반환

--- src/main/java/nbc/mushroom/domain/bid/repository/BidRepositoryCustom.java
+++ src/main/java/nbc/mushroom/domain/bid/repository/BidRepositoryCustom.java
@@ -1,5 +1,11 @@
 package nbc.mushroom.domain.bid.repository;
 
-public interface BidRepositoryCustom {
+import java.util.Optional;
+import nbc.mushroom.domain.bid.entity.Bid;
+import nbc.mushroom.domain.product.entity.Product;
+import nbc.mushroom.domain.user.entity.User;
 
+public interface BidRepositoryCustom {
+    
+    Optional<Bid> findBidByUserAndProduct(User bidder, Product product);
 }
