3a073e5d521b2d2f27d59e8c76a7edc3933867d6
yeongbinim <yeim.dev@gmail.com>
2025-02-11 17:59:40 +0900

    :bug: fix(auth): Validate password format

--- src/main/java/nbc/mushroom/domain/auth/dto/request/UserRegisterReq.java
+++ src/main/java/nbc/mushroom/domain/auth/dto/request/UserRegisterReq.java
@@ -2,6 +2,7 @@ package nbc.mushroom.domain.auth.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record UserRegisterReq(
     @NotBlank
@@ -9,6 +10,10 @@ public record UserRegisterReq(
     String email,
 
     @NotBlank
+    @Pattern(
+        regexp = "^(?=.*[0-9])(?=.*[A-Z]).{8,20}$",
+        message = "새 비밀번호는 8자 이상 20자 이하여야 하고, 숫자와 대문자를 포함해야 합니다."
+    )
     String password,
 
     @NotBlank
