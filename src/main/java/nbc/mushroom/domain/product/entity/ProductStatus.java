ae2de741ff1444ac66ad31ab26893ee28649b6c6
yeongbinim <yeim.dev@gmail.com>
2025-02-11 15:54:38 +0900

    :bug: fix: Add rejected status to `ProductStatus`

--- src/main/java/nbc/mushroom/domain/product/entity/ProductStatus.java
+++ src/main/java/nbc/mushroom/domain/product/entity/ProductStatus.java
@@ -2,6 +2,7 @@ package nbc.mushroom.domain.product.entity;
 
 public enum ProductStatus {
     INSPECTING,          // 검수중
+    REJECTED,            // 검수실패
     WAITING,             // 대기중
     PROGRESSING,         // 진행중
     COMPLETED            // 종료
