0fd3249b60886d24d7274b22f8103d4c4202f3a0
answerin1 <dabin990723@naver.com>
2025-02-12 00:10:35 +0900

    :sparkles: feat: Implement product detail view controller

--- src/main/java/nbc/mushroom/domain/product/controller/ProductControllerV1.java
+++ src/main/java/nbc/mushroom/domain/product/controller/ProductControllerV1.java
@@ -1,7 +1,13 @@
 package nbc.mushroom.domain.product.controller;
 
 import lombok.RequiredArgsConstructor;
+import nbc.mushroom.domain.common.dto.ApiResponse;
+import nbc.mushroom.domain.product.dto.response.searchProductRes;
 import nbc.mushroom.domain.product.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductControllerV1 {
 
     private final ProductService productService;
+
+    // 상품 상세 조회
+    @GetMapping("/{productId}")
+    public ResponseEntity<ApiResponse<searchProductRes>> searchProduct(
+        @PathVariable long productId) {
+        searchProductRes searchProductRes = productService.searchProductRes(productId);
+
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(ApiResponse.success("상품이 정상적으로 조회되었습니다.", searchProductRes));
+    }
+
 }
