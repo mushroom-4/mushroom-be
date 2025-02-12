f984d47ce8768d8a3111aa80816cae445ca2110f
duol9 <lhyy0195@gmail.com>
2025-02-12 09:55:00 +0900

    :sparkles: feat: Add createOrUpdateBid method to BidService
    - Bid를 찾고, 없으면 Bid 생성 후 BiddingPrice 수정

--- src/main/java/nbc/mushroom/domain/bid/service/BidService.java
+++ src/main/java/nbc/mushroom/domain/bid/service/BidService.java
@@ -1,7 +1,15 @@
 package nbc.mushroom.domain.bid.service;
 
 import lombok.RequiredArgsConstructor;
+import nbc.mushroom.domain.bid.dto.request.CreateBidReq;
+import nbc.mushroom.domain.bid.dto.response.CreateBidRes;
+import nbc.mushroom.domain.bid.entity.Bid;
 import nbc.mushroom.domain.bid.repository.BidRepository;
+import nbc.mushroom.domain.common.exception.CustomException;
+import nbc.mushroom.domain.common.exception.ExceptionType;
+import nbc.mushroom.domain.product.entity.Product;
+import nbc.mushroom.domain.product.repository.ProductRepository;
+import nbc.mushroom.domain.user.entity.User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,4 +19,20 @@ import org.springframework.transaction.annotation.Transactional;
 public class BidService {
 
     private final BidRepository bidRepository;
+    private final ProductRepository productRepository;
+
+    @Transactional(readOnly = false)
+    public CreateBidRes createOrUpdateBid(User loginUser, Long productId,
+        CreateBidReq createBidReq) {
+        Product findProduct = productRepository.findById(productId)
+            .orElseThrow(() -> new CustomException(
+                ExceptionType.PRODUCT_NOT_FOUND)); // 이 동작을 bid서비스에서 보여주고 싶지 않은데.. Product Repository에 디폴트 메서드 또는 Bid로 반환하는 메서드 생기면 변경하기로
+
+        Bid findBid = bidRepository.findBidByUserAndProduct(loginUser, findProduct)
+            .orElseGet(() -> createBid(loginUser, findProduct));
+
+        findBid.updateBiddingPrice(createBidReq.biddingPrice());
+
+        return CreateBidRes.from(findBid);
+    }
 }
