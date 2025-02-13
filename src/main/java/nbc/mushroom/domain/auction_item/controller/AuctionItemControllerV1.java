f336676291b38321333f797357816f5247745341
yeongbinim <yeim.dev@gmail.com>
2025-02-13 16:51:15 +0900

    :recycle: refactor: Change order of Controller Methods

--- src/main/java/nbc/mushroom/domain/auction_item/controller/AuctionItemControllerV1.java
+++ src/main/java/nbc/mushroom/domain/auction_item/controller/AuctionItemControllerV1.java
@@ -33,19 +33,6 @@ public class AuctionItemControllerV1 {
 
     private final AuctionItemService auctionItemService;
 
-    // 경매 물품 상세 조회
-    @GetMapping("/{auctionItemId}/info")
-    public ResponseEntity<ApiResponse<SearchAuctionItemRes>> searchAuctionItem(
-        @PathVariable long auctionItemId) {
-        SearchAuctionItemRes searchAuctionItemRes = auctionItemService.searchAuctionItem(
-            auctionItemId);
-
-        return ResponseEntity
-            .status(HttpStatus.OK)
-            .body(ApiResponse.success("경매 물품이 정상적으로 조회되었습니다.", searchAuctionItemRes));
-    }
-
-
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ApiResponse<AuctionItemRes>> postAuctionItem(
         @Valid @ModelAttribute CreateAuctionItemReq createAuctionItemReq,
@@ -57,6 +44,32 @@ public class AuctionItemControllerV1 {
 
         return ResponseEntity.status(HttpStatus.CREATED)
             .body(ApiResponse.success("경매 물품 등록에 성공했습니다.", auctionItemRes));
+    }
+
+    // 경매 물품 전체 조회 (페이징 조회 포함)
+    @GetMapping
+    public ResponseEntity<ApiResponse<Page<SearchAuctionItemRes>>> searchAllAuctionItems(
+        @RequestParam(value = "page", defaultValue = "1") int page
+    ) {
+        Pageable pageable = PageRequest.of(page - 1, 10);
+        Page<SearchAuctionItemRes> allAuctionItems = auctionItemService.findAllAuctionItems(
+            pageable);
+
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(ApiResponse.success("경매 물품이 전제 조회 되었습니다.", allAuctionItems));
+    }
+
+    // 경매 물품 상세 조회
+    @GetMapping("/{auctionItemId}/info")
+    public ResponseEntity<ApiResponse<SearchAuctionItemRes>> searchAuctionItem(
+        @PathVariable long auctionItemId) {
+        SearchAuctionItemRes searchAuctionItemRes = auctionItemService.searchAuctionItem(
+            auctionItemId);
+
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(ApiResponse.success("경매 물품이 정상적으로 조회되었습니다.", searchAuctionItemRes));
     }
 
     @PutMapping(value = "/{auctionItemId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
@@ -84,19 +97,5 @@ public class AuctionItemControllerV1 {
 
         return ResponseEntity.status(HttpStatus.NO_CONTENT)
             .body(ApiResponse.success("경매 물품 삭제에 성공했습니다."));
-    }
-
-    // 경매 물품 전체 조회 (페이징 조회 포함)
-    @GetMapping
-    public ResponseEntity<ApiResponse<Page<SearchAuctionItemRes>>> searchAllAuctionItems(
-        @RequestParam(value = "page", defaultValue = "1") int page
-    ) {
-        Pageable pageable = PageRequest.of(page - 1, 10);
-        Page<SearchAuctionItemRes> allAuctionItems = auctionItemService.findAllAuctionItems(
-            pageable);
-
-        return ResponseEntity
-            .status(HttpStatus.OK)
-            .body(ApiResponse.success("경매 물품이 전제 조회 되었습니다.", allAuctionItems));
     }
 }
