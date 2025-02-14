7694254d344dc1894665b3530fcb2586ec7b8abb
duol9 <lhyy0195@gmail.com>
2025-02-14 10:54:53 +0900

    :sparkles: feat: Add getUserBidDetail method to UserBidControllerV1

--- src/main/java/nbc/mushroom/domain/user/controller/UserBidControllerV1.java
+++ src/main/java/nbc/mushroom/domain/user/controller/UserBidControllerV1.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,5 +37,17 @@ public class UserBidControllerV1 {
 
         return ResponseEntity.status(HttpStatus.OK)
             .body(ApiResponse.success("입찰 내역 목록 조회에 성공했습니다", getUserBidRes));
+    }
+
+    @GetMapping("/{bidId}")
+    public ResponseEntity<ApiResponse<UserBidRes>> getUserBidDetail(
+        @Auth AuthUser authUser,
+        @PathVariable Long bidId
+    ) {
+        User loginUser = User.fromAuthUser(authUser);
+        UserBidRes userBidRes = userBidService.getUserBidDetail(loginUser, bidId);
+
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(ApiResponse.success("입찰 내역 상세 조회에 성공했습니다.", userBidRes));
     }
 }
