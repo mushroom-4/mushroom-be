d54dc1a893577b2d8972a69471ae4a2936f3adc9
TE <xodms128@naver.com>
2025-02-18 13:23:11 +0900

    :bug: fix: Fix repository , response dto

--- src/main/java/nbc/mushroom/domain/like/repository/LikeRepositoryImpl.java
+++ src/main/java/nbc/mushroom/domain/like/repository/LikeRepositoryImpl.java
@@ -3,19 +3,16 @@ package nbc.mushroom.domain.like.repository;
 import static nbc.mushroom.domain.auction_item.entity.QAuctionItem.auctionItem;
 import static nbc.mushroom.domain.like.entity.QLike.like;
 
-import com.querydsl.core.Tuple;
 import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.Wildcard;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import nbc.mushroom.domain.auction_item.entity.AuctionItem;
 import nbc.mushroom.domain.auction_item.entity.QAuctionItem;
-import nbc.mushroom.domain.common.exception.CustomException;
-import nbc.mushroom.domain.common.exception.ExceptionType;
 import nbc.mushroom.domain.like.entity.Like;
 import nbc.mushroom.domain.user.dto.response.SearchUserAuctionItemLikeRes;
 import nbc.mushroom.domain.user.entity.QUser;
@@ -45,9 +42,12 @@ public class LikeRepositoryImpl implements LikeRepositoryCustom {
     @Override
     public PageImpl<SearchUserAuctionItemLikeRes> findAuctionItemLikeByUserId(User user,
         Pageable pageable) {
-        // like.id, auctionItem 의 정보를 List 튜플 형태로 저장
-        List<Tuple> likeIdAutionItemTuples = Optional.ofNullable(
-            queryFactory.select(like.id, auctionItem)
+        // like, auctionItem 의 정보 담기
+        List<SearchUserAuctionItemLikeRes> searchUserAuctionItemLikeResList =
+            queryFactory.select(Projections.constructor(
+                    SearchUserAuctionItemLikeRes.class,
+                    like,
+                    auctionItem))
                 .from(like)
                 .innerJoin(like.user, QUser.user)
                 .innerJoin(like.auctionItem, auctionItem)
@@ -56,18 +56,7 @@ public class LikeRepositoryImpl implements LikeRepositoryCustom {
                 .orderBy(getSortOrders(pageable)) // 정렬 추가
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
-                .fetch()).orElseThrow(() -> new CustomException(ExceptionType.LIKE_NOT_FOUND));
-
-        // 저장한 List 튜플의 값들을 튜플로 꺼내서, 해당 값들을
-        // SearchUserAuctionItemLikeRes List 에 맞추어 저장
-        List<SearchUserAuctionItemLikeRes> searchUserAuctionItemLikeResList = new ArrayList<>();
-        for (Tuple likeIdAutionItemTuple : likeIdAutionItemTuples) {
-            SearchUserAuctionItemLikeRes searchUserAuctionItemLikeRes = SearchUserAuctionItemLikeRes.from(
-                likeIdAutionItemTuple.get(like.id),
-                Objects.requireNonNull(likeIdAutionItemTuple.get(auctionItem)));
-
-            searchUserAuctionItemLikeResList.add(searchUserAuctionItemLikeRes);
-        }
+                .fetch();
 
         Long totalCount = Optional.ofNullable(
             queryFactory.select(Wildcard.count)
--- src/main/java/nbc/mushroom/domain/user/dto/response/SearchUserAuctionItemLikeRes.java
+++ src/main/java/nbc/mushroom/domain/user/dto/response/SearchUserAuctionItemLikeRes.java
@@ -3,33 +3,30 @@ package nbc.mushroom.domain.user.dto.response;
 import java.time.LocalDateTime;
 import nbc.mushroom.domain.auction_item.entity.AuctionItem;
 import nbc.mushroom.domain.auction_item.entity.AuctionItemStatus;
+import nbc.mushroom.domain.like.entity.Like;
 
 public record SearchUserAuctionItemLikeRes(
 
     Long likeId,
     Long auctionItemId,
     String name,
-//    String description,
     String imageUrl,
-//    AuctionItemSize size,
-//    AuctionItemCategory category,
-//    String brand,
     Long startPrice,
     LocalDateTime startTime,
     LocalDateTime endTime,
     AuctionItemStatus status
 ) {
 
-    public static SearchUserAuctionItemLikeRes from(Long likeId, AuctionItem searchAuctionItem) {
-        return new SearchUserAuctionItemLikeRes(
-            likeId,
-            searchAuctionItem.getId(),
-            searchAuctionItem.getName(),
-            searchAuctionItem.getImageUrl(),
-            searchAuctionItem.getStartPrice(),
-            searchAuctionItem.getStartTime(),
-            searchAuctionItem.getEndTime(),
-            searchAuctionItem.getStatus()
+    public SearchUserAuctionItemLikeRes(Like like, AuctionItem AuctionItem) {
+        this(
+            like.getId(),
+            AuctionItem.getId(),
+            AuctionItem.getName(),
+            AuctionItem.getImageUrl(),
+            AuctionItem.getStartPrice(),
+            AuctionItem.getStartTime(),
+            AuctionItem.getEndTime(),
+            AuctionItem.getStatus()
         );
     }
 }
