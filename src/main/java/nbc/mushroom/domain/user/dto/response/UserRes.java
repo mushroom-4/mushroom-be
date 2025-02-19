c1e433e5a45cd8051cac1beacadde8538bc39ae2
yeongbinim <yeim.dev@gmail.com>
2025-02-19 14:57:41 +0900

    :bug: fix(user): Add imageUrl, nickname field to UserRes DTO

--- src/main/java/nbc/mushroom/domain/user/dto/response/UserRes.java
+++ src/main/java/nbc/mushroom/domain/user/dto/response/UserRes.java
@@ -4,10 +4,12 @@ import nbc.mushroom.domain.user.entity.User;
 
 public record UserRes(
     Long id,
-    String email
+    String email,
+    String nickname,
+    String imageUrl
 ) {
 
     public static UserRes from(User user) {
-        return new UserRes(user.getId(), user.getEmail());
+        return new UserRes(user.getId(), user.getEmail(), user.getNickname(), user.getImageUrl());
     }
 }
