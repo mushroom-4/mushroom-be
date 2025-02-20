e9179a50186c5f93950016a85f4b9e6863dd18ad
duol9 <lhyy0195@gmail.com>
2025-02-20 10:07:25 +0900

    :sparkles: feat: Configure message broker for STOMP

--- src/main/java/nbc/mushroom/config/websocket/WebSocketConfig.java
+++ src/main/java/nbc/mushroom/config/websocket/WebSocketConfig.java
@@ -2,6 +2,7 @@ package nbc.mushroom.config.websocket;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -27,5 +28,22 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws")
             .addInterceptors(jwtHandshakeInterceptor)
             .setAllowedOriginPatterns("*");
+    }
+
+    /**
+     * 메시지 브로커.
+     *
+     * 클라이언트가 메시지 주고 받을 경로 지정
+     * /pub: 클라이언트가 메시지를 보낼 때 사용하는 접두사 ( @MessageMapping과 연결. /pub + /@MessageMapping 엔드포인트 )
+     * /sub: 클라이언트가 구독할 수 있는 주제(topic)의 접두사, 메시지 브로커를 통해 해당 주소를 구독하는 클라이언트로 전달 (라우팅)
+     *
+     * @param registry
+     */
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes(
+            "/pub"); // 클라이언트 -> 서버
+        registry.enableSimpleBroker(
+            "/sub"); //  서버 -> 클라이언트
     }
 }
