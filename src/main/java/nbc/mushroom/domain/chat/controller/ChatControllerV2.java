d4820aa6b4d8d9a78088578ae6ffc1cfbb184405
duol9 <lhyy0195@gmail.com>
2025-02-23 22:41:04 +0900

    :sparkles: feat: Add getChatHistroy method to ChatControllerV2

--- src/main/java/nbc/mushroom/domain/chat/controller/ChatControllerV2.java
+++ src/main/java/nbc/mushroom/domain/chat/controller/ChatControllerV2.java
@@ -1,17 +1,23 @@
 package nbc.mushroom.domain.chat.controller;
 
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import nbc.mushroom.domain.chat.dto.request.ChatMessageReq;
 import nbc.mushroom.domain.chat.dto.response.ChatMessageRes;
 import nbc.mushroom.domain.chat.service.ChatService;
 import nbc.mushroom.domain.common.annotation.Auth;
+import nbc.mushroom.domain.common.dto.ApiResponse;
 import nbc.mushroom.domain.common.dto.AuthUser;
 import nbc.mushroom.domain.user.entity.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -30,5 +36,14 @@ public class ChatControllerV2 {
         log.info("Auth User ID : {} ", authUser.id());
         return chatService.sendChatMessage(chatRoomId, chatMessageReq,
             User.fromAuthUser(authUser));
+    }
+
+    @GetMapping("/api/v2/bids/chats/{chatRoomId}")
+    public ResponseEntity<ApiResponse<List<ChatMessageRes>>> getChatHistory(
+        @PathVariable Long chatRoomId
+    ) {
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(ApiResponse.success("이전 채팅 내역이 성공적으로 조회되었습니다.",
+                chatService.getChatHistory(chatRoomId)));
     }
 }
