26bff694e1ec15c0c4e6cb4308501743098ef903
yeongbinim <yeim.dev@gmail.com>
2025-02-25 17:02:22 +0900

    :recycle: refactor(auth): Unify versioning

--- src/main/java/nbc/mushroom/domain/auth/controller/AuthController.java
+++ src/main/java/nbc/mushroom/domain/auth/controller/AuthController.java
@@ -2,6 +2,7 @@ package nbc.mushroom.domain.auth.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import nbc.mushroom.domain.auth.dto.request.UserLoginReq;
 import nbc.mushroom.domain.auth.dto.request.UserRegisterReq;
 import nbc.mushroom.domain.auth.dto.response.TokenRes;
 import nbc.mushroom.domain.auth.service.AuthService;
@@ -11,13 +12,14 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v2/auth")
-public class AuthControllerV2 {
+@RequestMapping("/api/auth")
+public class AuthController {
 
     private final AuthService authService;
 
@@ -28,5 +30,14 @@ public class AuthControllerV2 {
         TokenRes tokenRes = authService.register(userRegisterReq);
         return ResponseEntity.status(HttpStatus.CREATED)
             .body(ApiResponse.success("정상적으로 회원가입 되었습니다.", tokenRes));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse<TokenRes>> login(
+        @Valid @RequestBody UserLoginReq userLoginReq
+    ) {
+        TokenRes tokenRes = authService.login(userLoginReq);
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(ApiResponse.success("정상적으로 로그인 되었습니다.", tokenRes));
     }
 }
