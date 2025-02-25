d0948cf299ec4d283a8cf824db0cb1b910b8ffca
yeongbinim <yeim.dev@gmail.com>
2025-02-25 16:29:17 +0900

    :recycle: refactor(chat): Add valid to CreateMessage

--- src/main/java/nbc/mushroom/domain/chat/dto/request/ChatMessageReq.java
+++ src/main/java/nbc/mushroom/domain/chat/dto/request/ChatMessageReq.java
@@ -1,9 +1,11 @@
 package nbc.mushroom.domain.chat.dto.request;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record ChatMessageReq(
 
+    @Size(max = 50)
     @NotNull(message = "메시지를 입력해주세요")
     String message
 ) {
