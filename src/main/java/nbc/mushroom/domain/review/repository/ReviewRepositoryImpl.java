478a4f62473d44436dd3821fbbe0dd0bd8b9ccdb
answerin1 <dabin990723@naver.com>
2025-02-25 09:57:17 +0900

    :sparkles: feat: Enhance review functionality in repository

--- src/main/java/nbc/mushroom/domain/review/repository/ReviewRepositoryImpl.java
+++ src/main/java/nbc/mushroom/domain/review/repository/ReviewRepositoryImpl.java
@@ -5,10 +5,9 @@ import static nbc.mushroom.domain.bid.entity.QBid.bid;
 import static nbc.mushroom.domain.review.entity.QReview.review;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import nbc.mushroom.domain.bid.entity.BiddingStatus;
 import nbc.mushroom.domain.review.entity.Review;
-import nbc.mushroom.domain.user.entity.User;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -18,19 +17,13 @@ public class ReviewRepositoryImpl implements ReviewRepositoryCustom {
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public Review findByBidderAndAuctionItem_Seller(User loginUser, Long sellerId) {
+    public List<Review> findAllBySellerId(Long sellerId) {
         return queryFactory
-            .select(review)
-            .from(review)
-            .join(review.bid, bid)
-            .join(bid.auctionItem, auctionItem)
-            .where(
-                auctionItem.seller.id.eq(sellerId),
-                bid.bidder.eq(loginUser),
-                bid.biddingStatus.eq(BiddingStatus.PAYMENT_COMPLETED)
-            )
-            .limit(1)
-            .fetchOne();
+            .selectFrom(review)
+            .innerJoin(review.bid, bid)
+            .innerJoin(bid.auctionItem, auctionItem)
+            .where(auctionItem.seller.id.eq(sellerId))
+            .fetch();
     }
 
 }
