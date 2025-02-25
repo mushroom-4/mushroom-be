66b6bfa699627b698f59cf1491d4f95f19dbe7da
answerin1 <dabin990723@naver.com>
2025-02-25 09:57:30 +0900

    :sparkles: feat: Enhance review functionality in service

--- src/main/java/nbc/mushroom/domain/review/service/ReviewService.java
+++ src/main/java/nbc/mushroom/domain/review/service/ReviewService.java
@@ -1,7 +1,7 @@
 package nbc.mushroom.domain.review.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import nbc.mushroom.domain.auction_item.entity.AuctionItem;
 import nbc.mushroom.domain.bid.entity.Bid;
 import nbc.mushroom.domain.bid.entity.BiddingStatus;
 import nbc.mushroom.domain.bid.repository.BidRepository;
@@ -9,6 +9,8 @@ import nbc.mushroom.domain.common.exception.CustomException;
 import nbc.mushroom.domain.common.exception.ExceptionType;
 import nbc.mushroom.domain.review.dto.request.CreateSellerReviewReq;
 import nbc.mushroom.domain.review.dto.response.CreateSellerReviewRes;
+import nbc.mushroom.domain.review.dto.response.SearchSellerReviewDetailRes;
+import nbc.mushroom.domain.review.dto.response.SearchSellerReviewRes;
 import nbc.mushroom.domain.review.entity.Review;
 import nbc.mushroom.domain.review.repository.ReviewRepository;
 import nbc.mushroom.domain.user.entity.User;
@@ -27,33 +29,29 @@ public class ReviewService {
     @Transactional
     public CreateSellerReviewRes createReview(
         User loginUser,
-        Long sellerId,
+        Long bidId,
         CreateSellerReviewReq createSellerReviewReq
     ) {
 
-        Review existingReview = reviewRepository.findByBidderAndAuctionItem_Seller(
-            loginUser, sellerId);
-        if (existingReview != null) {
+        Boolean existingReview = reviewRepository.existsByBidId(bidId);
+        if (existingReview) {
             throw new CustomException(
                 ExceptionType.REVIEW_ALREADY_EXISTS); // 구매자가 해당 판매자에 대해 이미 리뷰 작성한 경우 중복 불가
         }
 
-        if (loginUser.getId().equals(sellerId)) {
-            throw new CustomException(ExceptionType.SELLER_CANNOT_REVIEW); // 판매자 본인의 리뷰 불가
-        }
+        Bid bid = bidRepository.findById(bidId)
+            .orElseThrow(() -> new CustomException(ExceptionType.BID_NOT_FOUND));
 
-        BiddingStatus paymentCompleted = BiddingStatus.PAYMENT_COMPLETED;
-        Bid winningBid = bidRepository.findByAuctionItemAndBidderAndBiddingStatus(
-            sellerId, loginUser, paymentCompleted);
-
-        if (winningBid == null) {
+        if (bid.getBiddingStatus() != BiddingStatus.PAYMENT_COMPLETED) {
             throw new CustomException(ExceptionType.BID_NO_PAYMENT); // 낙찰 + 결제완료인 입찰자만 리뷰 작성에 해당
         }
 
-        AuctionItem auctionItem = winningBid.getAuctionItem();
+        if (!bid.getBidder().getId().equals(loginUser.getId())) {
+            throw new CustomException(ExceptionType.INVALID_REVIEW_USER);
+        }
 
         Review review = Review.builder()
-            .bid(winningBid)                  // 결제 완료 입찰인 상태
+            .bid(bid)                  // 결제 완료 상태인 입찰
             .score(createSellerReviewReq.score())
             .content(createSellerReviewReq.content())
             .build();
@@ -61,6 +59,26 @@ public class ReviewService {
         reviewRepository.save(review);
 
         return CreateSellerReviewRes.from(review);
+    }
+
+    // 리뷰 조회 (판매자 한명의 리뷰 리스트)
+    public SearchSellerReviewRes searchReviewsBySeller(Long sellerId) {
+
+        List<Review> reviews = reviewRepository.findAllBySellerId(sellerId);
+
+        // 평균 점수 계산
+        Double averageScore = reviews.stream()
+            .mapToInt(Review::getScore)
+            .average()
+            .orElse(0.0);
+
+        // 리뷰룰 DTO로 변환
+        List<SearchSellerReviewDetailRes> reviewDetails = reviews.stream()
+            .map(SearchSellerReviewDetailRes::from)
+            .toList();
+
+        // 평균 점수와 리뷰 세부정보를 포함한 결과 반환
+        return new SearchSellerReviewRes(averageScore, reviewDetails);
     }
 }
 
