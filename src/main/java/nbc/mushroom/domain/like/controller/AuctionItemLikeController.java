41ac4efcf6a87812117e1a98733657471493bdd3
TE <xodms128@naver.com>
2025-02-26 11:29:19 +0900

    :bug: fix: Change api uri and code

--- src/main/java/nbc/mushroom/domain/like/controller/AuctionItemLikeController.java
+++ src/main/java/nbc/mushroom/domain/like/controller/AuctionItemLikeController.java
@@ -4,13 +4,10 @@ import lombok.RequiredArgsConstructor;
 import nbc.mushroom.domain.common.annotation.Auth;
 import nbc.mushroom.domain.common.dto.ApiResponse;
 import nbc.mushroom.domain.common.dto.AuthUser;
-import nbc.mushroom.domain.like.dto.response.CheckLikedAuctionItemRes;
 import nbc.mushroom.domain.like.service.AuctionItemLikeService;
-import nbc.mushroom.domain.user.entity.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -47,19 +44,4 @@ public class AuctionItemLikeController {
             .body(ApiResponse.success("정상적으로 좋아요가 취소되었습니다."));
     }
 
-
-    // 본인이 해당 경매 물품에, 좋아요를 한 여부를 확인해주는 API
-    @GetMapping("/{auction-items_id}/likes")
-    public ResponseEntity<ApiResponse<CheckLikedAuctionItemRes>> checkLikedAuctionItem(
-        @Auth AuthUser authUser,
-        @PathVariable("auction-items_id") Long auctionItemId
-    ) {
-        User user = User.fromAuthUser(authUser);
-        CheckLikedAuctionItemRes auctionItems = auctionItemLikeService.getLikedAuctionItem(
-            user, auctionItemId);
-
-        return ResponseEntity.status(HttpStatus.OK)
-            .body(ApiResponse.success("해당 경매 물품에 유저의 좋아요 여부를 확인했습니다.",
-                auctionItems));
-    }
 }
--- src/main/java/nbc/mushroom/domain/like/service/AuctionItemLikeService.java
+++ src/main/java/nbc/mushroom/domain/like/service/AuctionItemLikeService.java
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import nbc.mushroom.domain.auction_item.entity.AuctionItem;
 import nbc.mushroom.domain.auction_item.repository.AuctionItemRepository;
 import nbc.mushroom.domain.common.exception.CustomException;
-import nbc.mushroom.domain.like.dto.response.CheckLikedAuctionItemRes;
 import nbc.mushroom.domain.like.entity.AuctionItemLike;
 import nbc.mushroom.domain.like.repository.AuctionItemLikeRepository;
 import nbc.mushroom.domain.user.entity.User;
@@ -46,15 +45,6 @@ public class AuctionItemLikeService {
             .build();
 
         auctionItemLikeRepository.save(auctionItemLike);
-    }
-
-    // 본인이 해당 경매 물품에, 좋아요를 한 여부를 확인
-    public CheckLikedAuctionItemRes getLikedAuctionItem(User user, Long auctionItemId) {
-
-        Boolean hasLike = auctionItemLikeRepository.existAuctionItemLikeByUserAndAuctionItem(
-            user, auctionItemId);
-
-        return new CheckLikedAuctionItemRes(hasLike);
     }
 
     public void hardDeleteAuctionItemLike(Long userId, Long auctionItemId) {
