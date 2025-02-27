3cb5b82ac077cc5d055585d187062ef0526c9038
yeongbinim <yeim.dev@gmail.com>
2025-02-27 12:28:03 +0900

    :recycle: refactor(bid): Move to AuctionItemRes DTO in BidRes

--- src/main/java/nbc/mushroom/domain/bid/dto/response/BidRes.java
+++ src/main/java/nbc/mushroom/domain/bid/dto/response/BidRes.java
@@ -1,6 +1,5 @@
 package nbc.mushroom.domain.bid.dto.response;
 
-import nbc.mushroom.domain.auction_item.dto.response.SearchAuctionItemRes;
 import nbc.mushroom.domain.bid.entity.Bid;
 import nbc.mushroom.domain.bid.entity.BiddingStatus;
 
@@ -8,7 +7,7 @@ public record BidRes(
     Long bidId,
     Long biddingPrice,
     BiddingStatus biddingStatus,
-    SearchAuctionItemRes searchAuctionItemRes
+    AuctionItemRes auctionItem
 ) {
 
     public static BidRes from(Bid bid) {
@@ -16,7 +15,17 @@ public record BidRes(
             bid.getId(),
             bid.getBiddingPrice(),
             bid.getBiddingStatus(),
-            SearchAuctionItemRes.from(bid.getAuctionItem())
+            new BidRes.AuctionItemRes(
+                bid.getAuctionItem().getName(),
+                bid.getAuctionItem().getImageUrl()
+            )
         );
+    }
+
+    private record AuctionItemRes(
+        String name,
+        String imageUrl
+    ) {
+
     }
 }
