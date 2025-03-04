5ba72ccf8e2fa605b35cc1ef6c8de8f4ea12d0c2
duol9 <lhyy0195@gmail.com>
2025-03-05 02:02:54 +0900

    :sparkles: feat: Add sendConcurrentUserList method to ChatRoomService

--- src/main/java/nbc/mushroom/domain/chat/service/ChatRoomService.java
+++ src/main/java/nbc/mushroom/domain/chat/service/ChatRoomService.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import nbc.mushroom.domain.chat.constant.RedisChatRoomKey;
+import nbc.mushroom.domain.chat.dto.response.ConcurrentUserListRes;
 import nbc.mushroom.domain.chat.dto.response.ConcurrentUserListRes.UserInfoRes;
 import nbc.mushroom.domain.user.entity.User;
 import nbc.mushroom.domain.user.repository.UserRepository;
@@ -21,6 +22,15 @@ public class ChatRoomService {
     private final RedisTemplate<String, Object> redisTemplate;
     private final UserRepository userRepository;
     private final RedisPublish redisPublish;
+
+    // 현재 접속자 목록 전송
+    public void sendConcurrentUserList(Long chatRoomId) {
+        List<UserInfoRes> userInfoResList = getConcurrentUsers(chatRoomId.toString());
+        ConcurrentUserListRes concurrentUserListRes = ConcurrentUserListRes.from(chatRoomId,
+            userInfoResList);
+
+        redisPublish.publishConcurrentUserList(concurrentUserListRes);
+    }
 
     // Redis에서 현재 접속자 목록을 가져옴
     private List<UserInfoRes> getConcurrentUsers(String chatRoomId) {
